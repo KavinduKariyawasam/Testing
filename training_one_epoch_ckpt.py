6cb78ba11e376ecb8fa523a8049116a4b5b8dfa0
Deependra Dewagiri <56537975+deepdewdeep@users.noreply.github.com>
2023-08-11 16:17:24 +0530

    Update training_one_epoch_ckpt.py

--- training_one_epoch_ckpt.py
+++ training_one_epoch_ckpt.py
@@ -1,11 +1,11 @@
 import torch
-from utils.utils import AverageMeter,warmup_learning_rate,accuracy
+from utils import AverageMeter,warmup_learning_rate,accuracy
 import sys
 import time
 import numpy as np
 from sklearn.metrics import precision_score,recall_score
-from config.config_linear import parse_option
-from utils.utils import set_loader_new, set_model, set_optimizer, adjust_learning_rate
+from config_linear import parse_option
+from utils import set_loader_new, set_model, set_optimizer, adjust_learning_rate
 def main():
     best_acc = 0
     opt = parse_option()
